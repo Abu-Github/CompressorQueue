f853a03a237b1b7e6dbca14185bcd91b11d55afc
Danish <danish@ultroid.tech>
2022-06-26 00:36:50 +0000

    pyLint: auto-fixes

--- bot/config.py
+++ bot/config.py
@@ -16,11 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int, default='1906076')
-    API_HASH = config("API_HASH", default='3be910cde04b3f810cd843880b24f737')
-    BOT_TOKEN = config("BOT_TOKEN", default='5306377919:AAGxkSjlbHKx1yDdA1lCmHvH5ePXSznv2n4')
+    APP_ID = config("APP_ID", cast=int, default="1906076")
+    API_HASH = config("API_HASH", default="3be910cde04b3f810cd843880b24f737")
+    BOT_TOKEN = config(
+        "BOT_TOKEN", default="5306377919:AAGxkSjlbHKx1yDdA1lCmHvH5ePXSznv2n4"
+    )
     DEV = 682111519
-    OWNER = config("OWNER",default='682111519')
+    OWNER = config("OWNER", default="682111519")
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -b:v 450k -map 0:v -c:a libvorbis -b:a 32k -ac 6 -map 0:a -c:s copy -map 0:s? "{}"',
